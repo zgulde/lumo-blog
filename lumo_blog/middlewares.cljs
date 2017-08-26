e7d256bd9e0ece4d10154c2a4c0e34b8cf48bf21
Zach Gulde <zachgulde@gmail.com>
2017-08-26 11:57:10 -0500

    add post access control as middleware

--- lumo_blog/middlewares.cljs
+++ lumo_blog/middlewares.cljs
@@ -10,4 +10,9 @@
   (if req.session.user_id
     (next)
     (do (.status res 403)
-        (.json res (clj->js {:error "Forbidden"})))))
+        (.json res (clj->js {:error "Please log in"})))))
+
+(defn post-access-control
+  [req res next]
+  (if-not (= req.params.id req.session.user_id)
+    (do (.status res 403) (.json res (clj->js {:error "you don't have permission for that resource"})))))
