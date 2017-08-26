c08d8643c102a54481640f59c02a9914be90b82a
Zach Gulde <zachgulde@gmail.com>
2017-08-26 18:09:33 -0500

    Add seeder

--- lumo_blog/db/seeder.cljs
+++ lumo_blog/db/seeder.cljs
@@ -1,13 +1,46 @@
-(ns lumo-blog.seeder
+(ns lumo-blog.db.seeder
   (:require [lumo-blog.db.user :as user]
             [lumo-blog.db.post :as post]
-            [lumo-blog.db.core :as db]))
+            [lumo-blog.db.core :as db]
+            [lumo-blog.util :as util]))
 
 (def users
-  [{:email "zach@codeup.com" :password "codeup"}])
+  [{:email "zach@codeup.com" :password "codeup"}
+   {:email "test@gmail.com" :password "codeup"}])
 
-(doseq [user users]
-  (user/insert user))
+(def posts
+  [{:title "" :body ""}])
 
-(js/setTimeout #(.end db/connection) 1500)
+(defn seed-users []
+  (util/log-info "seeding users...")
+  (js/Promise.
+    (fn [resolve reject]
+      (let [pending-requests (atom 0)
+            done (fn [] (swap! pending-requests dec)
+                   (when (= 0 @pending-requests)
+                     (resolve)))]
+        (doseq [user users]
+          (swap! pending-requests inc)
+          (.then (user/insert user) done))))))
 
+(defn seed-posts [user-id]
+  (util/log-info "seeding posts...")
+  (js/Promise.
+    (fn [resolve reject]
+      (let [pending-requests (atom 0)
+            done (fn [] (swap! pending-requests dec)
+                   (when (= 0 @pending-requests) (resolve)))]
+        (doseq [post posts]
+          (swap! pending-requests inc)
+          (.then (post/insert post) done))))))
+
+(defn run []
+  (util/ps (seed-users)
+           (fn [] (user/by-email "zach@codeup.com"))
+           (fn [user] (seed-posts (:id user)))))
+
+(defn -main []
+  (util/log-info "Starting Seeder...")
+  (.then (run)
+         (fn [] (.end db/connection) (util/log-success "Seeder finished!"))
+         (fn [err] (util/log-error err))))
