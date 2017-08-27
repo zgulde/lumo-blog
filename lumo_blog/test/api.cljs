05fb88f2992769d5e3fc63e2a68334bc640d8640
Zach Gulde <zachgulde@gmail.com>
2017-08-27 11:06:31 -0500

    Test routes should be restricted to authed users

--- lumo_blog/test/api.cljs
+++ lumo_blog/test/api.cljs
@@ -1,9 +1,14 @@
+; 19 21 41 42 62
+
 (ns lumo-blog.test.api
   (:require [cljs.test :as test]
             [lumo-blog.server :refer [configure-app]]
-            [lumo-blog.util :refer [log-info]]
-            [lumo-blog.db.core :as db]))
+            [lumo-blog.util :as util]
+            [lumo-blog.db.core :as db]
+            [lumo-blog.db.seeder :as seeder]
+            [lumo-blog.db.migration :as migration]))
 
+;; request default options
 (def request (.defaults (js/require "request")
                         (clj->js {:jar true
                                   :baseUrl "http://localhost:1312"
@@ -11,44 +16,45 @@
 
 (def express (js/require "express"))
 (def app (configure-app (express)))
-(def server (.listen app 1312 #(log-info "server started on port 1312!")))
-
-(def pending-requests (atom 0))
-
-(defn- done []
-  (when (= 1 @pending-requests)
-    (log-info "All done, cleaning up...")
-    (.close server)
-    (.end db/connection))
-  (swap! pending-requests dec))
+(def server (.listen app 1312 #(util/log-info "server started on port 1312!")))
 
 (defn- req [opts cb]
-  (swap! pending-requests inc)
-  (request (clj->js opts)
-           (fn [err res body]
-             (cb err res (js->clj body :keywordize-keys true)))))
+  (js/Promise.
+    (fn [resolve reject]
+      (request (clj->js opts)
+               (fn [err res body]
+                 (if err
+                   (reject err)
+                   (resolve [res (js->clj body :keywordize-keys true)])))))))
 
-(defn test-restricted [method url]
-  (req {:method method :url url}
-       (fn [err res body]
-         (test/is (= 403 res.statusCode))
-         (test/is (string? (:error body)))
-         (done))))
+(defn test-route-is-restricted [method url]
+  (.then (req {:method method :url url})
+         (fn [[res body]]
+           (test/is (= 403 res.statusCode) (str method " to " url " gives 403"))
+           (test/is (string? (:error body)) (str method " to " url " has an error field in the response body")))))
 
-(test/deftest authentication
+; (defn- login [f]
+;   (req {:method "post" :url "/login" :body {:email "zach@codeup.com"
+;                                             :password "codeup"}}
+;        (fn [err res body]
+;          (if (= 200 res.statusCode)
+;            (f)
+;            (util/log-error "failed to login! " body)))))
 
-  (test/testing "/account"
-    (test-restricted "get" "/account"))
+(defn test-authentication []
+  (.all js/Promise
+        [(test-route-is-restricted "get" "/account")
+         (test-route-is-restricted "post" "/logout")
+         (test-route-is-restricted "post" "/posts")
+         (test-route-is-restricted "put" "/posts/1")
+         (test-route-is-restricted "delete" "/posts/1")]))
 
-  (test/testing "/posts"
-    (test-restricted "post" "/posts")
-    (req {:method "get" :url "/posts"}
-         (fn [err res body]
-           (let [id (:id (first body))]
-             (test-restricted "put" (str "/posts/" id))
-             (test-restricted "delete" (str "/posts/" id)))
-           (done)))
-    )
-  )
-
-(authentication)
+(defn -main []
+  (util/log-info "Starting...")
+  (.catch (util/ps (migration/run)
+                   #(seeder/run)
+                   #(test-authentication)
+                   #(.end db/connection)
+                   #(.close server)
+                   #(util/log-success "All Done!"))
+          (fn [err] (util/log-error err))))
