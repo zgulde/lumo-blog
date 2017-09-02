35dd307fcec818e87c8fca1d5d5588ad9911ae69
Zach Gulde <zachgulde@gmail.com>
2017-09-02 11:26:12 -0500

    Add `merge` utility function

---
+++ src/util.js
@@ -0,0 +1,2 @@
+export const merge = (...objects) =>
+  Object.assign({}, ...objects)
---
+++ src/util.test.js
@@ -0,0 +1,26 @@
+import {merge} from './util'
+
+describe('merge(...objects)', () => {
+  it('Should be defined', () => {
+    expect(merge).toBeDefined()
+  })
+  it('Merges multiple objects', () => {
+    expect(merge({foo: 'bar'}, {baz: 'pony'}, {pony: 'quux'})).toEqual({
+      foo: 'bar',
+      baz: 'pony',
+      pony: 'quux'
+    })
+  })
+  it('Uses the last passed object if there are duplicate keys', () => {
+    expect(merge({foo: 'bar'}, {foo: 'pony'})).toEqual({
+      foo: 'pony'
+    })
+  })
+  it('Does not mutate the passed objects (Object.assign mutates the first argument)', () => {
+    const object1 = {foo: 'bar'}
+    const object2 = {baz: 'pony'}
+    merge(object1, object2)
+    expect(object1).toEqual({foo: 'bar'})
+    expect(object2).toEqual({baz: 'pony'})
+  })
+})
