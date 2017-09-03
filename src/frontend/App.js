063e7749a4ae37374440eeffb4ce35bde1b11ce6
Zach Gulde <zachgulde@gmail.com>
2017-09-02 20:41:36 -0500

    Fix style

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
-export default () => (
+const App = () => (
   <div>
     <h1>Hello, World!</h1>
   </div>
 )
+
+export default App
--- src/frontend/App.test.js
+++ src/frontend/App.test.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import render from 'react-dom'
 import { shallow } from 'enzyme'
 import App from './App'
 
 describe('<App />', () => {
-  it("renders without crashing",() => {
+  it('renders without crashing', () => {
     expect(shallow(<App />).text()).toMatch(/hello,\sworld/i)
   })
 })
