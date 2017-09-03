df222a4a1f0572a0d20336b091a3fb67494e0f5f
Zach Gulde <zachgulde@gmail.com>
2017-09-02 20:44:59 -0500

    Setup reducer to handle promise-based actions

--- src/frontend/reducers.js
+++ src/frontend/reducers.js
@@ -1,13 +1,29 @@
+import {merge} from './util'
+
 export const initialState = {
-  loggedIn: false
+  loggedIn: false,
+  pendingRequests: {
+    login: false,
+    logout: false,
+    posts: false
+  }
 }
 
 export const rootReducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'LOGIN':
-      return Object.assign({}, state, {loggedIn: true})
-    case 'LOGOUT':
-      return Object.assign({}, state, {loggedIn: false})
+    case 'LOGIN_PENDING':
+      return merge(state, {pendingRequests: {login: true}})
+    case 'LOGIN_FULFILLED':
+    case 'LOGIN_REJECTED':
+      return merge(state, {
+        pendingRequests: {login: false},
+        loggedIn: action.payload.success
+      })
+    case 'LOGOUT_PENDING':
+      return merge(state, {pendingRequests: {logout: true}})
+    case 'LOGOUT_FULFILLED':
+    case 'LOGOUT_REJECTED':
+      return merge(state, {pendingRequests: {logout: false}})
     default:
       return state
   }
--- src/frontend/reducers.test.js
+++ src/frontend/reducers.test.js
@@ -1,20 +1,41 @@
-import {rootReducer} from './reducers'
-import * as actions from './actions'
+import {rootReducer, initialState} from './reducers'
+import {merge} from './util'
 
 describe('rootReducer', () => {
   it('returns the default state with an empty action', () => {
-    expect(rootReducer(undefined, {})).toEqual({
-      loggedIn: false
-    })
+    expect(rootReducer(undefined, {})).toEqual(initialState)
   })
-  it('handles the login action', () => {
-    expect(rootReducer(undefined, actions.login())).toEqual({
-      loggedIn: true
-    })
+})
+describe('LOGIN actions', () => {
+  const loginSuccess = {type: 'LOGIN_FULFILLED', payload: {success: true}}
+  const loginFailed = {type: 'LOGIN_FULFILLED', payload: {success: false}}
+  it('handles _PENDING', () => {
+    const state = rootReducer(initialState, {type: 'LOGIN_PENDING'})
+    expect(state.pendingRequests.login).toBe(true)
   })
-  it('handles the logout function', () => {
-    expect(rootReducer({loggedIn: true}, actions.logout())).toEqual({
-      loggedIn: false
-    })
+  it('request is no longer pending on _FULFILLED or _REJECTED', () => {
+    const initial = merge(initialState, {pendingRequests: {login: true}})
+    expect(initial.pendingRequests.login).toBe(true)
+    const state = rootReducer(initial, loginSuccess)
+    expect(state.pendingRequests.login).toBe(false)
+  })
+  it('sets the loggedIn key based off of the payload of _FULFILLED', () => {
+    let state = rootReducer(initialState, loginSuccess)
+    expect(state.loggedIn).toBe(true)
+    state = rootReducer(initialState, loginFailed)
+    expect(state.loggedIn).toBe(false)
+  })
+})
+
+describe('LOGOUT actions', () => {
+  it('handles _PENDING', () => {
+    const state = rootReducer(initialState, {type: 'LOGOUT_PENDING'})
+    expect(state.pendingRequests.logout).toBe(true)
+  })
+  it('request is no longer pending on _FULFILLED or _REJECTED', () => {
+    const initial = merge(initialState, {pendingRequests: {logout: true}})
+    expect(initial.pendingRequests.logout).toBe(true)
+    const state = rootReducer(initial, {type: 'LOGOUT_FULFILLED'})
+    expect(state.pendingRequests.logout).toBe(false)
   })
 })
